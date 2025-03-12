43e7351ce1c40cb6042b764f1f9446f2d4c13ec6
Pitiwat Promnimit <pitiwat.p@ooca.co>
2025-03-12 08:40:04 +0700

    chore: update ApiTags annotation to use consistent casing for 'Auth'

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,7 +3,7 @@ import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { ApiTags } from '@nestjs/swagger';
 
-@ApiTags('auth')
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
